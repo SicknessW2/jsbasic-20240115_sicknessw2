1d3fbc2a209b13924ac4e867a761e95666be285e
SicknessW2 <sicknessw2@icloud.com>
2024-02-28 04:55:38 +0500

    модули 2 и 3

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,9 @@
 function isEmpty(obj) {
-  // ваш код...
+  let count = 0;
+
+  for (let key in obj) {
+    count++;
+  }
+  
+  return count == 0;
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+    const numbers = str
+    .split(' ')
+    .map(value => +value)
+    .filter(value => !isNaN(value));
+
+    const result = {
+    	min: Math.min(...numbers),
+    	max: Math.max(...numbers)
+    }
+	return result
 }
